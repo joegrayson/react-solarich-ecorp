1ed0aed0a2c2d076cdb9c98f8f31661578c065b2
joegrayson <kingjoelunas@gmail.com>
2023-09-12 19:55:54 +0800

    feat: testing a loading spinner feature

--- solarich-ecorp/src/App.js
+++ solarich-ecorp/src/App.js
@@ -1,15 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 //Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+//Components
+import Loader from "./components/Loader";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <div>
+      {isLoading ? (
+        // Loading screen component
+        <Loader />
+      ) : (
+        // Render your routes when loading is complete
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      )}
+    </div>
   );
 }
 
