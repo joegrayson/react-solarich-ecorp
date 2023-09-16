fe17b7cd09303422c4848b15a9cec58e6f94206d
joegrayson <kingjoelunas@gmail.com>
2023-09-16 19:04:41 +0800

    style: padding adjustments in about page

--- solarich-ecorp/src/pages/About.js
+++ solarich-ecorp/src/pages/About.js
@@ -75,7 +75,7 @@ const About = () => {
       {/* <!-- Objectives Section --> */}
 
       <section className="bg-gray-50 bg-opacity-85 ">
-        <div className="container py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-6">
+        <div className="container px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-6">
           <div className="grid max-w-screen-xl gap-8 sm:gap-x-20 lg:grid-cols-2 sm:mx-auto mb-12">
             <div className="grid grid-cols-2 gap-5">
               <img
@@ -147,7 +147,7 @@ const About = () => {
       {/* <!-- Mission Vision Section --> */}
 
       <section className="bg-gray-50 bg-opacity-85 ">
-        <div className="container py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="container px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <h2 className="max-w-2xl mb-6 font-sans text-3xl font-bold leading-none tracking-normal text-green-900 sm:text-5xl md:mx-auto custom-font-2">
               <span className="relative inline-block">
@@ -258,7 +258,7 @@ const About = () => {
       {/* <!-- Core Values Section --> */}
 
       <section className="bg-gray-50 bg-opacity-85">
-        <div className="container py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
+        <div className="container px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-normal text-green-900 sm:text-5xl md:mx-auto custom-font-2">
               <span className="relative inline-block">
