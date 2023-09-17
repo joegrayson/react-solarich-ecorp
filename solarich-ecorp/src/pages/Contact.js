af13d99f7b877d4de61b0f7a227e28489b5a7097
joegrayson <kingjoelunas@gmail.com>
2023-09-18 00:07:03 +0800

    style: added shadow-lg and drop-shadow-lg to map iframe

--- solarich-ecorp/src/pages/Contact.js
+++ solarich-ecorp/src/pages/Contact.js
@@ -46,7 +46,7 @@ const Contact = () => {
       </section>
       <ContactSection />
       <section>
-        <div className="mt-10 px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-28">
+        <div className="mt-10 px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pb-16">
           <Map />
         </div>
       </section>
