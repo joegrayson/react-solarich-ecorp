85ad55b0ed9df37e26b2d1f01b4bf7924c2864bc
joegrayson <kingjoelunas@gmail.com>
2023-09-23 02:13:19 +0800

    style: padding adjustments in mission vision section in about page

--- solarich-ecorp/src/sections/about/MissionVision.js
+++ solarich-ecorp/src/sections/about/MissionVision.js
@@ -4,7 +4,7 @@ const MissionVision = () => {
   return (
     <Fragment>
       <section className="bg-gray-50 bg-opacity-85 ">
-        <div className="container px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="container px-8 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
             <h2 className="max-w-2xl mb-6 font-sans text-3xl font-bold leading-none tracking-normal text-green-900 sm:text-5xl md:mx-auto custom-font-2">
               <span className="relative inline-block">
