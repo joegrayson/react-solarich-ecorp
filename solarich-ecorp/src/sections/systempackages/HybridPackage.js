1b24db3cf72ba5493f85ee8c21e1552d3a37f9d9
joegrayson <kingjoelunas@gmail.com>
2023-09-23 01:55:21 +0800

    style: padding adjustments in system packages page

--- solarich-ecorp/src/sections/systempackages/HybridPackage.js
+++ solarich-ecorp/src/sections/systempackages/HybridPackage.js
@@ -5,7 +5,7 @@ const HybridPackage = () => {
   return (
     <Fragment>
       <section className="bg-gray-50 bg-opacity-85">
-        <div className="container px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="container px-8 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="p-8 rounded-lg border-2 border-green-300 shadow-lg sm:p-16 py-12">
             <div className="flex flex-col lg:flex-row">
               <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
--- solarich-ecorp/src/sections/systempackages/OnGridPackage.js
+++ solarich-ecorp/src/sections/systempackages/OnGridPackage.js
@@ -5,7 +5,7 @@ const OnGridPackage = () => {
   return (
     <Fragment>
       <section className="bg-gray-50 bg-opacity-85">
-        <div className="mt-10 container px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-28">
+        <div className="mt-10 container px-8 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-28">
           <div className="max-w-xl mb-6 md:mx-auto text-center sm:text-center lg:max-w-2xl md:mb-8">
             <h2 className="custom-font-2 max-w-xl sm:max-w-2xl mb-4 font-sans text-3xl font-bold tracking-normal text-green-900 sm:text-5xl sm:leading-none custom-font-2">
               <span className="relative inline-block">
